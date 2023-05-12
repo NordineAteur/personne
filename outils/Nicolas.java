b00059b5e896ea88b6d7339a07a7114f116278a3
utopia3500 <nicolas.junker@hotmail.com>
2023-05-12 12:44:45 +0200

    commit de l ajout de la classe Nicolas avec la méthode exercice

---
+++ outils/Nicolas.java
@@ -0,0 +1,17 @@
+package outils;
+
+import outils.modules.Fonctions;
+
+public class Nicolas {
+    /* Dans cette classe, ajouter une méthode nommée 
+     * exercie qui utilise la méthode ecrire de la
+     * classe Fonctions pour afficher "bonjour je m'appelle ..."
+     * 
+     * Dans le fichier Main.java, appelez la méthode exercice
+     * qui est déclarée dans votre classe.
+     */
+
+     public void exercice(){
+        Fonctions.ecrire("Bonjour je m'apelle Nicolas");
+     }
+}
